caa765d9bdcd446139b9c774307fac86fbe94478
ZeZhong Zhang <zezhong.zhang@autodesk.com>
2017-12-19 17:33:04 +0800

    Test

--- main.go
+++ main.go
@@ -13,23 +13,22 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+	"unsafe"
 
 	"github.com/coyove/goflyway/pkg/logg"
 )
 
-type readState struct {
-	buf []byte
-	n   int
-	err error
-	idx uint32
-}
-
-const readRespChanSize = 256
-const acceptStreamChanSize = 256
-const bufferSize = 32*1024 - 1
-const version = 137
+const (
+	readRespChanSize     = 256
+	acceptStreamChanSize = 256
+	bufferSize           = 32*1024 - 1 // 0x7fff
+	streamTimeout        = 20
+	version              = 0x89
+)
 
 const (
+	cmdByte = 0xff
+
 	cmdHello = iota
 	cmdAck
 	cmdErr
@@ -42,18 +41,23 @@ var (
 	errStreamLost = errors.New("dial: no answer from the remote")
 )
 
-type StreamMap struct {
-	sync.RWMutex
-	m map[uint32]*Stream
+type Map32 struct {
+	*sync.RWMutex
+	m map[uint32]unsafe.Pointer
 }
 
-func (sm *StreamMap) Store(id uint32, s *Stream) {
+func (Map32) New() Map32 {
+	return Map32{RWMutex: new(sync.RWMutex), m: make(map[uint32]unsafe.Pointer)}
+}
+
+// Store accepts v only if it is a pointer
+func (sm *Map32) Store(id uint32, v interface{}) {
 	sm.Lock()
-	sm.m[id] = s
+	sm.m[id] = (*[2]unsafe.Pointer)(unsafe.Pointer(&v))[1]
 	sm.Unlock()
 }
 
-func (sm *StreamMap) Delete(ids ...uint32) {
+func (sm *Map32) Delete(ids ...uint32) {
 	sm.Lock()
 	for _, id := range ids {
 		delete(sm.m, id)
@@ -61,14 +65,14 @@ func (sm *StreamMap) Delete(ids ...uint32) {
 	sm.Unlock()
 }
 
-func (sm *StreamMap) Load(id uint32) (*Stream, bool) {
+func (sm *Map32) Load(id uint32) (unsafe.Pointer, bool) {
 	sm.RLock()
 	s, ok := sm.m[id]
 	sm.RUnlock()
 	return s, ok
 }
 
-func (sm *StreamMap) First() (s *Stream) {
+func (sm *Map32) First() (s unsafe.Pointer) {
 	sm.RLock()
 	for _, s = range sm.m {
 		break
@@ -77,18 +81,19 @@ func (sm *StreamMap) First() (s *Stream) {
 	return
 }
 
-func (sm *StreamMap) Len() int {
+func (sm *Map32) Len() int {
 	sm.RLock()
 	ln := len(sm.m)
 	sm.RUnlock()
 	return ln
 }
 
-func (sm *StreamMap) Iterate(f func(id uint32, s *Stream) bool) {
+// Iterate deletes the entry if its callback returns false
+func (sm *Map32) Iterate(callback func(id uint32, s unsafe.Pointer) bool) {
 	sm.RLock()
 	ids := []uint32{}
 	for k, v := range sm.m {
-		if !f(k, v) {
+		if !callback(k, v) {
 			ids = append(ids, k)
 		}
 	}
@@ -99,22 +104,41 @@ func (sm *StreamMap) Iterate(f func(id uint32, s *Stream) bool) {
 	}
 }
 
+// IterateConst breaks when callback returns false
+func (sm *Map32) IterateConst(callback func(id uint32, s unsafe.Pointer) bool) {
+	sm.RLock()
+	for k, v := range sm.m {
+		if !callback(k, v) {
+			break
+		}
+	}
+	sm.RUnlock()
+}
+
+type readState struct {
+	buf []byte
+	n   int
+	err error
+	idx uint32
+	cmd byte
+}
+
 type Stream struct {
 	master *connState
 
-	stream uint32
+	streamIdx uint32
 
-	readResp chan *readState
-
-	writeStateResp chan byte
-	readStateResp  chan byte
-
-	closed              atomic.Value
-	readExit, writeExit chan bool
-
+	readResp        chan *readState
 	readOverflowBuf []byte
 
-	tag byte
+	writeStateResp chan byte // state returned from remote when writing
+
+	shouldClose bool
+	closed      atomic.Value
+	readExit    chan bool // inform Read() to exit
+	writeExit   chan bool // inform Write() to exit
+
+	tag byte // for debug purpose
 
 	lastActive int64
 	timeout    int64
@@ -122,14 +146,13 @@ type Stream struct {
 
 func newStream(id uint32, c *connState) *Stream {
 	s := &Stream{
-		stream:         id,
+		streamIdx:      id,
 		master:         c,
 		readExit:       make(chan bool, 1),
 		writeExit:      make(chan bool, 1),
 		readResp:       make(chan *readState, readRespChanSize),
 		writeStateResp: make(chan byte, 1),
-		readStateResp:  make(chan byte, 1),
-		timeout:        100,
+		timeout:        streamTimeout,
 		lastActive:     time.Now().UnixNano(),
 	}
 
@@ -155,13 +178,24 @@ func (c *Stream) Read(buf []byte) (n int, err error) {
 		return
 	}
 
+	atomic.StoreInt64(&c.lastActive, time.Now().UnixNano())
+
+	// MORE:
 	select {
-	case cmd := <-c.readStateResp:
-		if cmd == cmdClose {
-			close(c.readStateResp)
-			return 0, errConnClosed
-		}
+	// case cmd := <-c.readStateResp:
+	// 	if cmd == cmdClose {
+	// 		if len(c.readResp) == 0 {
+	// 			return 0, errConnClosed
+	// 		}
+	// 		c.shouldClose = true
+	// 	}
+
+	// 	goto MORE
 	case x := <-c.readResp:
+		// if x.cmd == cmdClose {
+		// 	return 0, errConnClosed
+		// }
+
 		n, err = x.n, x.err
 
 		if x.buf != nil {
@@ -188,7 +222,7 @@ func (c *Stream) Write(buf []byte) (n int, err error) {
 	}
 
 	header := make([]byte, 7+len(buf))
-	binary.BigEndian.PutUint32(header[1:], uint32(c.stream))
+	binary.BigEndian.PutUint32(header[1:], uint32(c.streamIdx))
 	binary.BigEndian.PutUint16(header[5:], uint16(len(buf)))
 	header[0] = version
 
@@ -212,69 +246,88 @@ func (c *Stream) Write(buf []byte) (n int, err error) {
 	atomic.StoreInt64(&c.lastActive, time.Now().UnixNano())
 	select {
 	case cmd := <-c.writeStateResp:
-		close(c.writeStateResp)
 		switch cmd {
 		case cmdErr:
 			return 0, errors.New("write: remote returns an error")
 		case cmdClose:
-			return 0, errors.New("write: remote has been closed")
+			return 0, errConnClosed
 		}
 	case <-c.writeExit:
 		return 0, errConnClosed
 	default:
 	}
 
+	atomic.StoreInt64(&c.lastActive, time.Now().UnixNano())
 	n -= 7
 	return
 }
 
 func (c *Stream) closeNoInfo() {
-	c.readExit <- true
-	c.writeExit <- true
+	select {
+	case c.readExit <- true:
+	default:
+	}
+
+	select {
+	case c.writeExit <- true:
+	default:
+	}
+
 	c.closed.Store(true)
 }
 
+// Close closes the stream and remove it from its master
 func (c *Stream) Close() error {
 	c.closeNoInfo()
 
-	buf := []byte{version, 0, 0, 0, 0, 0xff, cmdClose}
-	binary.BigEndian.PutUint32(buf[1:], c.stream)
+	buf := []byte{version, 0, 0, 0, 0, cmdByte, cmdClose}
+	binary.BigEndian.PutUint32(buf[1:], c.streamIdx)
 	if _, err := c.master.conn.Write(buf); err != nil {
 		c.master.broadcast(err)
 	}
 
+	c.master.streams.Delete(c.streamIdx)
 	return nil
 }
 
+// SetTimeout sets the timeout for this stream, error range: 1 sec
+func (c *Stream) SetTimeout(sec int64) {
+	atomic.StoreInt64(&c.timeout, sec)
+}
+
+// LocalAddr is a compatible method for net.Conn
 func (c *Stream) LocalAddr() net.Addr { return &net.TCPAddr{} }
 
-func (c *Stream) RemoteAddr() net.Addr { return &net.TCPAddr{} }
+// RemoteAddr is a compatible method for net.Conn
+func (c *Stream) RemoteAddr() net.Addr { return c.master.address }
 
+// SetReadDeadline is a compatible method for net.Conn
+func (c *Stream) SetReadDeadline(t time.Time) error { return c.SetDeadline(t) }
+
+// SetWriteDeadline is a compatible method for net.Conn
+func (c *Stream) SetWriteDeadline(t time.Time) error { return c.SetDeadline(t) }
+
+// SetDeadline is a compatible method for net.Conn
 func (c *Stream) SetDeadline(t time.Time) error {
 	sec := (t.UnixNano() - time.Now().UnixNano() + 1e6) / 1e9
 	if sec <= 0 {
+		c.SetTimeout(0)
 		return nil
 	}
-
-	atomic.StoreInt64(&c.timeout, sec)
+	c.SetTimeout(sec)
 	return nil
 }
 
-func (c *Stream) SetReadDeadline(t time.Time) error { return c.SetDeadline(t) }
-
-func (c *Stream) SetWriteDeadline(t time.Time) error { return c.SetDeadline(t) }
-
-func (c *Stream) cleanup() {
-}
-
 type connState struct {
-	master map[int64]*connState
-	idx    int64
+	address *net.TCPAddr
 
-	conn      net.Conn
-	exitRead  chan bool
-	streams   StreamMap
-	maxStream uint32
+	master  Map32
+	streams Map32
+
+	idx uint32
+
+	conn     net.Conn
+	exitRead chan bool
 
 	newStreamCallback func(state *readState)
 
@@ -282,15 +335,17 @@ type connState struct {
 	sync.Mutex
 }
 
+// When something serious happened, we broadcast it to every stream and close the master conn
+// TCP connections may have temporary errors, but here we treat them as the same as other failures
 func (cs *connState) broadcast(err error) {
 	logg.E("master conn err: ", err)
 
-	cs.streams.Iterate(func(idx uint32, s *Stream) bool {
-		s.readResp <- &readState{err: err}
+	cs.streams.Iterate(func(idx uint32, s unsafe.Pointer) bool {
+		(*Stream)(s).readResp <- &readState{err: err}
 		return true
 	})
 
-	cs.stop(true)
+	cs.stop()
 }
 
 func (cs *connState) start() {
@@ -305,21 +360,22 @@ func (cs *connState) start() {
 				return
 			default:
 				now := time.Now().UnixNano()
-				cs.streams.Iterate(func(idx uint32, s *Stream) bool {
+				cs.streams.Iterate(func(idx uint32, p unsafe.Pointer) bool {
+					s := (*Stream)(p)
 					if s.closed.Load().(bool) {
 						// return false to delete
 						return false
 					}
 
-					if (now-s.lastActive)/1e9 <= atomic.LoadInt64(&s.timeout) {
+					if to := atomic.LoadInt64(&s.timeout); to > 0 && (now-s.lastActive)/1e9 <= to {
 						return true
 					}
 
-					//s.Close()
+					s.closeNoInfo()
 					return false
 				})
 				//logg.D(">>>>>>", cs.streams.Len())
-				if _, err := cs.conn.Write([]byte{version, 0, 0, 0, 0, 0xff, cmdPing}); err != nil {
+				if _, err := cs.conn.Write([]byte{version, 0, 0, 0, 0, cmdByte, cmdPing}); err != nil {
 					cs.broadcast(err)
 					return
 				}
@@ -338,36 +394,38 @@ func (cs *connState) start() {
 			}
 
 			if buf[0] != version {
-				cs.broadcast(errors.New("invalid header received"))
+				cs.broadcast(errors.New("fatal: invalid header received"))
 				return
 			}
 
 			streamIdx := binary.BigEndian.Uint32(buf[1:])
 			streamLen := int(binary.BigEndian.Uint16(buf[5:]))
-			if buf[5] == 0xff {
+			if buf[5] == cmdByte {
 				switch buf[6] {
 				case cmdHello:
 					cs.newStreamCallback(&readState{idx: streamIdx})
 
-					buf[5], buf[6] = 0xff, cmdAck
+					buf[5], buf[6] = cmdByte, cmdAck
 					// we acknowledge the hello
 					if _, err = cs.conn.Write(buf); err != nil {
 						cs.broadcast(err)
 						return
 					}
+
 					fallthrough
 				case cmdPing:
 					readChan <- true
 					return
 				default:
-					if s, ok := cs.streams.Load(streamIdx); ok {
+					if p, ok := cs.streams.Load(streamIdx); ok {
+						s := (*Stream)(p)
 						select {
 						case s.writeStateResp <- buf[6]:
 						default:
-							logg.D(buf[6])
 						}
+
 						select {
-						case s.readStateResp <- buf[6]:
+						case s.readResp <- &readState{cmd: buf[6]}:
 						default:
 						}
 					}
@@ -389,9 +447,9 @@ func (cs *connState) start() {
 			}
 
 			if s, ok := cs.streams.Load(streamIdx); ok {
-				s.readResp <- rs
+				(*Stream)(s).readResp <- rs
 			} else {
-				buf[5], buf[6] = 0xff, cmdClose
+				buf[5], buf[6] = cmdByte, cmdClose
 				if _, err = cs.conn.Write(buf); err != nil {
 					cs.broadcast(err)
 					return
@@ -416,7 +474,7 @@ func (cs *connState) start() {
 	}
 }
 
-func (cs *connState) stop(gracefully bool) {
+func (cs *connState) stop() {
 	cs.Lock()
 	if cs.stopped {
 		cs.Unlock()
@@ -424,14 +482,14 @@ func (cs *connState) stop(gracefully bool) {
 	}
 
 	cs.exitRead <- true
-	cs.streams.Iterate(func(idx uint32, s *Stream) bool {
-		logg.D("connState is closing: ", s.stream)
+	cs.streams.Iterate(func(idx uint32, p unsafe.Pointer) bool {
+		s := (*Stream)(p)
 		s.closeNoInfo()
 		return true
 	})
 
 	cs.conn.Close()
-	delete(cs.master, cs.idx) //TODO
+	cs.master.Delete(cs.idx)
 
 	cs.stopped = true
 	cs.Unlock()
@@ -443,53 +501,44 @@ type DialPool struct {
 	maxConns int
 
 	address string
-	conns   map[int64]*connState
+	conns   Map32
 
-	connsCtr  int64
-	streamCtr int64
+	connsCtr  uint32
+	streamCtr uint32
 }
 
-func NewDialPool(addr string, max int) *DialPool {
+func NewDialer(addr string, poolSize int) *DialPool {
 	dp := &DialPool{
 		address:  addr,
-		maxConns: max,
-		conns:    make(map[int64]*connState),
+		maxConns: poolSize,
+		conns:    Map32{}.New(),
 	}
-
-	// go func() {
-	// 	for {
-	// 		for idx, cs := range dp.conns {
-	// 			logg.D("conn", idx)
-	// 			for _, s := range cs.streams {
-	// 				logg.D("    stream", s.stream)
-	// 			}
-	// 		}
-	// 		time.Sleep(5 * time.Second)
-	// 	}
-	// }()
 
 	return dp
 }
 
 func (d *DialPool) Dial() (net.Conn, error) {
-	d.Lock()
+	return d.DialTimeout(0)
+}
 
-	counter := func() uint32 {
-		c := uint32(atomic.AddInt64(&d.streamCtr, 1))
-		if c == 0 {
-			c = uint32(atomic.AddInt64(&d.streamCtr, 1))
-			// c may not be 1, but it's ok
-		}
-		return c
+func (d *DialPool) DialTimeout(timeout time.Duration) (net.Conn, error) {
+	if d.maxConns == 0 {
+		return net.DialTimeout("tcp", d.address, timeout)
 	}
 
 	newStreamAndSayHello := func(c *connState) (*Stream, error) {
-		s := newStream(counter(), c)
-		s.tag = 'c'
-		c.streams.Store(s.stream, s)
+		counter := atomic.AddUint32(&d.streamCtr, 1)
+		if counter == 0 {
+			counter = atomic.AddUint32(&d.streamCtr, 1)
+			// counter may not be 1, but it's ok
+		}
 
-		buf := []byte{version, 0, 0, 0, 0, 0xff, cmdHello}
-		binary.BigEndian.PutUint32(buf[1:], s.stream)
+		s := newStream(counter, c)
+		s.tag = 'c'
+		c.streams.Store(s.streamIdx, s)
+
+		buf := []byte{version, 0, 0, 0, 0, cmdByte, cmdHello}
+		binary.BigEndian.PutUint32(buf[1:], s.streamIdx)
 		_, err := c.conn.Write(buf)
 
 		if err != nil {
@@ -504,22 +553,22 @@ func (d *DialPool) Dial() (net.Conn, error) {
 				return nil, errStreamLost
 			}
 		}
+
 		return s, nil
 	}
 
-	if len(d.conns) < d.maxConns {
-		c := &connState{exitRead: make(chan bool), streams: StreamMap{m: make(map[uint32]*Stream)}, master: d.conns}
-		d.connsCtr++
-		ctr := d.connsCtr
-		c.idx = ctr
-		d.conns[ctr] = c
-		d.Unlock()
+	// not thread-safe, maybe we will have connections more than maxConns
+	if d.conns.Len() < d.maxConns {
+		c := &connState{exitRead: make(chan bool), streams: Map32{}.New(), master: d.conns}
+		c.address, _ = net.ResolveTCPAddr("tcp", d.address)
 
-		conn, err := net.Dial("tcp", d.address)
+		ctr := atomic.AddUint32(&d.connsCtr, 1)
+		c.idx = ctr
+		d.conns.Store(ctr, c)
+
+		conn, err := net.DialTimeout("tcp", d.address, timeout)
 		if err != nil {
-			d.Lock()
-			delete(d.conns, ctr)
-			d.Unlock()
+			d.conns.Delete(ctr)
 			return nil, err
 		}
 
@@ -529,21 +578,24 @@ func (d *DialPool) Dial() (net.Conn, error) {
 		return newStreamAndSayHello(c)
 	}
 
-	defer d.Unlock()
-
-	ln, _, conn := len(d.conns), int64(0), (*connState)(nil)
+	conn := (*connState)(nil)
 	for try := 0; conn == nil || conn.conn == nil; try++ {
-		i := 0
-		for _, conn = range d.conns {
-			if rand.Intn(ln-i) == 0 && conn.conn != nil {
-				break
+		i, ln := 0, d.conns.Len()
+
+		// ln may change
+		d.conns.IterateConst(func(id uint32, p unsafe.Pointer) bool {
+			conn = (*connState)(p)
+			if ln-i > 0 && rand.Intn(ln-i) == 0 && conn.conn != nil {
+				// break
+				return false
 			}
 			i++
-		}
+			return true
+		})
 
-		// if try > 100 {
-		// 	return nil, errors.New("can't fina a valid connection, try again later")
-		// }
+		if try > 1e6 {
+			logg.W("too many tries of finding a valid conn")
+		}
 	}
 
 	return newStreamAndSayHello(conn)
@@ -552,11 +604,11 @@ func (d *DialPool) Dial() (net.Conn, error) {
 type ListenPool struct {
 	ln net.Listener
 
-	conns   map[int64]*connState
-	streams StreamMap
+	conns   Map32
+	streams Map32
 
-	connsCtr  int64
-	streamCtr int64
+	connsCtr  uint32
+	streamCtr uint32
 
 	newStreamWaiting chan uint32
 
@@ -564,18 +616,23 @@ type ListenPool struct {
 	exitA chan bool
 }
 
-func NewListenPool(addr string) (*ListenPool, error) {
+func Listen(addr string, pooling bool) (net.Listener, error) {
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
 
+	if !pooling {
+		return ln, err
+	}
+
 	lp := &ListenPool{
-		ln:               ln,
-		exit:             make(chan bool, 1),
-		exitA:            make(chan bool, 1),
-		conns:            make(map[int64]*connState),
-		streams:          StreamMap{m: make(map[uint32]*Stream)},
+		ln:      ln,
+		exit:    make(chan bool, 1),
+		exitA:   make(chan bool, 1),
+		conns:   Map32{}.New(),
+		streams: Map32{}.New(),
+
 		newStreamWaiting: make(chan uint32, acceptStreamChanSize),
 	}
 
@@ -591,7 +648,7 @@ func (l *ListenPool) accept() {
 		default:
 			conn, err := l.ln.Accept()
 			if err != nil {
-				logg.D("accept:", err)
+				logg.D("accept: ", err)
 				continue
 			}
 
@@ -602,7 +659,7 @@ func (l *ListenPool) accept() {
 				conn:     conn,
 				master:   l.conns,
 				exitRead: make(chan bool),
-				streams:  StreamMap{m: make(map[uint32]*Stream)},
+				streams:  Map32{}.New(),
 				newStreamCallback: func(state *readState) {
 					idx := state.idx
 					s := newStream(idx, c)
@@ -611,14 +668,10 @@ func (l *ListenPool) accept() {
 					c.streams.Store(idx, s)
 					l.streams.Store(idx, s)
 					l.newStreamWaiting <- idx
-
-					if idx > c.maxStream {
-						c.maxStream = idx
-					}
 				},
 			}
 
-			l.conns[l.connsCtr] = c
+			l.conns.Store(l.connsCtr, c)
 			go c.start()
 		}
 	}
@@ -628,7 +681,7 @@ func (l *ListenPool) Accept() (net.Conn, error) {
 	select {
 	case idx := <-l.newStreamWaiting:
 		s, _ := l.streams.Load(idx)
-		return s, nil
+		return (*Stream)(s), nil
 	case <-l.exitA:
 		return nil, errors.New("listener has ended")
 	}
@@ -645,14 +698,29 @@ func (l *ListenPool) Addr() net.Addr {
 }
 
 func main() {
+	// f, _ := os.Open("z.txt")
+	// buf := make([]byte, 102400)
+	// f.Read(buf)
+	// fmt.Println(string(buf))
+	// if true {
+	// 	return
+	// }
+
 	logg.SetLevel("dbg")
 	logg.Start()
 
 	go func() {
-		ln, _ := NewListenPool(":13739")
+		ln, _ := Listen(":13739", true)
 
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte(r.RequestURI[1:]))
+			logg.D("hibegin")
+			h := w.(http.Hijacker)
+			conn, _, _ := h.Hijack()
+			logg.D("hiend")
+
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n" + r.RequestURI[1:]))
+			conn.Close()
+			// w.Write([]byte(r.RequestURI[1:]))
 		})
 		http.Serve(ln, nil)
 
@@ -681,7 +749,7 @@ func main() {
 	}()
 
 	logg.D("z")
-	p := NewDialPool("127.0.0.1:13739", 1)
+	p := NewDialer("127.0.0.1:13739", 1)
 
 	client := http.Client{
 		Transport: &http.Transport{
@@ -718,9 +786,9 @@ func main() {
 
 	go func() {
 		for {
-			time.Sleep(time.Minute)
+			time.Sleep(30 * time.Second)
 			//p.conns[1].conn.Close()
-			f, _ := os.Create("1.txt")
+			f, _ := os.Create("heap.txt")
 			pprof.Lookup("goroutine").WriteTo(f, 1)
 		}
 		//time.Sleep(time.Second * 5)
@@ -730,20 +798,21 @@ func main() {
 	for {
 		wg := &sync.WaitGroup{}
 
-		start := time.Now()
+		//start := time.Now()
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go test(wg)
 		}
 		wg.Wait()
-		logg.D("take: ", time.Now().Sub(start).Seconds())
+		//logg.D("take: ", time.Now().Sub(start).Seconds())
 
 		sctr := 0
-		for _, c := range p.conns {
-			sctr += c.streams.Len()
-		}
+		p.conns.IterateConst(func(id uint32, p unsafe.Pointer) bool {
+			sctr += (*connState)(p).streams.Len()
+			return true
+		})
 
-		logg.D(len(p.conns), " ", sctr)
+		logg.D(p.conns.Len(), " ", sctr)
 	}
 
 	select {}
